0ab10e4003c34be5ac4e7304b9ae602748778e95
Divikiran Ravela <divikiran.ravela@kmart.com.au>
2019-02-20 12:56:53 +1100

    Viewmodel binding on mainpage - not working

---
+++ ShellFreshh/ShellFreshh/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FreshMvvm;
+
+namespace ShellFreshh
+{
+    public class MainViewModel : FreshBasePageModel
+    {
+        public MainViewModel()
+        {
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+        }
+
+        protected override void ViewIsDisappearing(object sender, EventArgs e)
+        {
+            base.ViewIsDisappearing(sender, e);
+        }
+    }
+}
